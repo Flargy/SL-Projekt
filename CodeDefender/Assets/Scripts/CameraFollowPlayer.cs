5175df11dc672b05ca3b2f1ad52ede3986b9340b
Flargy <marlun1996@hotmail.com>
2019-09-27 11:08:32 +0200

    camera fix

--- CodeDefender/Assets/Scripts/CameraFollowPlayer.cs
+++ CodeDefender/Assets/Scripts/CameraFollowPlayer.cs
@@ -10,22 +10,43 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField] private float heightOffset = 12.5f;
     [SerializeField] private float xDistanceFromPlayer = 0;
     [SerializeField] private float xRotation;
+    
 
     private bool lerping = false;
     private Quaternion oldRotation;
     private Quaternion newRotation;
+    private float newXDistance;
+    private float newZDistance;
+    private float lerpedXDistance;
+    private float lerpedZDistance;
+    private Vector3 lerpVector;
 
     void Update()
     {
-        transform.position = new Vector3((player.transform.position.x - xDistanceFromPlayer), (player.transform.position.y + heightOffset), (player.transform.position.z - zDistanceFromPlayer));
         if (lerping)
         {
             transform.rotation = Quaternion.Lerp(oldRotation, newRotation, 0.2f * Time.time);
-            Debug.Log(oldRotation + "  " + newRotation);
+            lerpedXDistance = Mathf.Lerp(xDistanceFromPlayer, newXDistance, 0.2f * Time.time);
+            lerpedZDistance = Mathf.Lerp(zDistanceFromPlayer, newZDistance, 0.2f * Time.time);
+            lerpVector = new Vector3(player.transform.position.x - lerpedXDistance, (player.transform.position.y + heightOffset), player.transform.position.z - lerpedZDistance);
+            transform.position = lerpVector;
+            Debug.Log(lerpedZDistance);
+            Debug.Log(lerpedXDistance);
             if (transform.rotation == newRotation)
             {
+                Debug.Log(lerpVector);
+                Debug.Log(newXDistance);
+                Debug.Log(newZDistance);
+                zDistanceFromPlayer = newZDistance;
+                xDistanceFromPlayer = newXDistance;
                 lerping = false;
+
             }
+        }
+        else if(lerping == false)
+        {
+            transform.position = new Vector3((player.transform.position.x - xDistanceFromPlayer), (player.transform.position.y + heightOffset), (player.transform.position.z - zDistanceFromPlayer));
+
         }
     }
 
@@ -35,8 +56,10 @@ public class CameraFollowPlayer : MonoBehaviour
         newRotation = Quaternion.Euler(xRotation, rotation, 0);
         oldRotation = transform.rotation;
         //transform.rotation = Quaternion.Euler(xRotation, rotation, 0);
-        zDistanceFromPlayer = zDistance;
-        xDistanceFromPlayer = xDistance;
+        newZDistance = zDistance;
+        newXDistance = xDistance;
+        lerpedXDistance = xDistanceFromPlayer;
+        lerpedZDistance = zDistanceFromPlayer;
     }
 
     
